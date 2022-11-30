7a4941034a03f9b3d964d893ec0111715ae841fd
Mark Canlas <github@htmlism.com>
2022-11-30 10:34:46 -0500

    cats-effect 3.4.2

--- project/GitHubActionsSettings.scala
+++ project/GitHubActionsSettings.scala
@@ -1,5 +1,4 @@
 import sbt._
-import sbt.Keys._
 import sbtghactions._
 import sbtghactions.GenerativeKeys._
 
@@ -17,17 +16,9 @@ object GitHubActionsSettings extends AutoPlugin {
     GitHubActionsPlugin
 
   override val buildSettings: Seq[Setting[_]] = Seq(
-    organization := "com.htmlism",
-    versionScheme := Some("strict"),
-    publishTo := Some("GitHub" at "https://maven.pkg.github.com/mcanlas/rufio/"),
-    credentials += Credentials(
-      "GitHub Package Registry",
-      "maven.pkg.github.com",
-      "mcanlas",
-      System.getenv("GITHUB_TOKEN")
-    ),
-    githubWorkflowBuild := Seq(WorkflowStep.Sbt(List("scalafmtCheck", "test"))),
-    githubWorkflowEnv := Map("GITHUB_TOKEN" -> "${{ secrets.WRITE_PACKAGES_TOKEN }}"),
+    githubWorkflowBuild := Seq(WorkflowStep.Sbt(List("scalafixAll --check", "scalafmtCheck", "test"))),
+    githubWorkflowEnv := Map.empty,
+    githubWorkflowPublishTargetBranches := Nil,
     githubWorkflowIncludeClean := false
   )
 }
