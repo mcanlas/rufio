35c7b930f67f3dbd4dd924ee3cf78e140a2b433a
Mark Canlas <github@htmlism.com>
2022-12-01 20:45:00 -0500

    fmt

--- project/GitHubActionsSettings.scala
+++ project/GitHubActionsSettings.scala
@@ -17,17 +17,17 @@ object GitHubActionsSettings extends AutoPlugin {
     GitHubActionsPlugin
 
   override val buildSettings: Seq[Setting[_]] = Seq(
-    organization := "com.htmlism",
-    versionScheme := Some("strict"),
-    publishTo := Some("GitHub" at "https://maven.pkg.github.com/mcanlas/rufio/"),
+    organization               := "com.htmlism",
+    versionScheme              := Some("strict"),
+    publishTo                  := Some("GitHub" at "https://maven.pkg.github.com/mcanlas/rufio/"),
     credentials += Credentials(
       "GitHub Package Registry",
       "maven.pkg.github.com",
       "mcanlas",
       System.getenv("GITHUB_TOKEN")
     ),
-    githubWorkflowBuild := Seq(WorkflowStep.Sbt(List("scalafmtCheck", "test"))),
-    githubWorkflowEnv := Map("GITHUB_TOKEN" -> "${{ secrets.WRITE_PACKAGES_TOKEN }}"),
+    githubWorkflowBuild        := Seq(WorkflowStep.Sbt(List("scalafmtCheck", "test"))),
+    githubWorkflowEnv          := Map("GITHUB_TOKEN" -> "${{ secrets.WRITE_PACKAGES_TOKEN }}"),
     githubWorkflowIncludeClean := false
   )
 }
