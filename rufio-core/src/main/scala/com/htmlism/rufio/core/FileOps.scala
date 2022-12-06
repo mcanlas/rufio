7e4487e2b6fa8989a7ec1c30807acd88bd5cd19a
Mark Canlas <github@htmlism.com>
2022-12-06 13:24:59 -0500

    doc

--- rufio-core/src/main/scala/com/htmlism/rufio/core/FileOps.scala
+++ rufio-core/src/main/scala/com/htmlism/rufio/core/FileOps.scala
@@ -1,9 +1,19 @@
 package com.htmlism.rufio.core
 
 trait FileOps[F[_]] {
+
+  /**
+    * Get the contents of the file as one `String`
+    */
   def contents: F[String]
 
+  /**
+    * Get the contents of the file, separated by newlines
+    */
   def getLines: F[List[String]]
 
+  /**
+    * Write a `String` to a file, ending with a newline
+    */
   def writeLine(s: String): F[Unit]
 }
