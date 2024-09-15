ec1a90f07c2eb03f3093a266a75aeb1adfd28077
Mark Canlas <github@htmlism.com>
2024-09-15 13:12:44 -0400

    :steam_locomotive: factor stdout

--- rufio-cats/src/test/scala/com/htmlism/rufio/cats/io/syntax/CatsIoSyntaxSuite.scala
+++ rufio-cats/src/test/scala/com/htmlism/rufio/cats/io/syntax/CatsIoSyntaxSuite.scala
@@ -5,11 +5,14 @@ import weaver.SimpleIOSuite
 import com.htmlism.rufio.core.Path
 
 object CatsIoSyntaxSuite extends SimpleIOSuite {
+  private lazy val out =
+    cats.effect.std.Console.make
+
   test("Can create a temporary file") {
     for {
       f <- Path.createTemporaryFile
 
-      _ <- cats.effect.std.Console.make.println(f)
+      _ <- out.println(f)
 
       b <- f.exists
     } yield expect(b)
