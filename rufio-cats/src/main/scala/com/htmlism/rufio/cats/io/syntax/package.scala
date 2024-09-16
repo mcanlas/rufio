96397f7f4e8eef9a8d3a931669e172b2f6937527
Mark Canlas <github@htmlism.com>
2024-09-15 20:28:46 -0400

    :ocean: use sync class

--- rufio-cats/src/main/scala/com/htmlism/rufio/cats/io/syntax/package.scala
+++ rufio-cats/src/main/scala/com/htmlism/rufio/cats/io/syntax/package.scala
@@ -2,13 +2,11 @@ package com.htmlism.rufio.cats.io
 
 import cats.effect.IO
 
+import com.htmlism.rufio.cats.sync.syntax.*
 import com.htmlism.rufio.core.*
 
 package object syntax {
-  private lazy val ioThunker: Thunker[IO] =
-    com.htmlism.rufio.cats.sync.syntax.syncThunker[IO]
+  implicit class InstanceOpsIo(path: Path) extends InstanceOpsSync[IO](path)
 
-  implicit class InstanceOpsIo(path: Path) extends PathInstanceOps(path, ioThunker)
-
-  implicit class CompanionOpsIo(obj: Path.type) extends PathCompanionOps(ioThunker)
+  implicit class CompanionOpsIo(obj: Path.type) extends CompanionOpsSync[IO](obj)
 }
