15b8d144633a567384d4882de3a76fa92cad1845
Mark Canlas <github@htmlism.com>
2024-09-15 20:21:23 -0400

    :lock: add get/set permissions

--- rufio-core/src/main/scala/com/htmlism/rufio/core/PathInstanceOps.scala
+++ rufio-core/src/main/scala/com/htmlism/rufio/core/PathInstanceOps.scala
@@ -1,10 +1,28 @@
 package com.htmlism.rufio.core
 
 import java.nio.file.Files
+import java.nio.file.attribute.PosixFilePermission
+
+import scala.jdk.CollectionConverters.*
 
 class PathInstanceOps[F[_]](path: Path, thunker: Thunker[F]) {
   def exists: F[Boolean] =
     thunker.blocking {
       Files.exists(path)
+    }
+
+  def getPosixFilePermissions: F[Set[PosixFilePermission]] =
+    thunker.blocking {
+      Files
+        .getPosixFilePermissions(path)
+        .asScala
+        .toSet
+    }
+
+  // `add` equivalent does not exist because it would require sequencing/FlatMap, not available for a non-cats F
+  def setPosixFilePermissions(permissions: Set[PosixFilePermission]): F[Path] =
+    thunker.blocking {
+      Files
+        .setPosixFilePermissions(path, permissions.asJava)
     }
 }
