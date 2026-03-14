cc74bc26a297681f09d906d53f3d51cc2a512d29
Mark Canlas <github@htmlism.com>
2026-03-13 21:38:39 -0400

    dependencies :magnet:

---
+++ project/DependenciesPlugin.scala
@@ -0,0 +1,30 @@
+import sbt.Keys.*
+import sbt.*
+
+object DependenciesPlugin extends AutoPlugin {
+  override def trigger = allRequirements
+
+  object autoImport {
+    implicit class DependencyOps(p: Project) {
+      def withCats: Project =
+        p
+          .settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.6.3")
+
+      def withZio: Project =
+        p
+          .settings(libraryDependencies += "dev.zio" %% "zio" % "2.1.9")
+
+      def withTesting: Project = {
+        val weaverVersion =
+          "0.10.0"
+
+        p.settings(
+          libraryDependencies ++= Seq(
+            "org.typelevel" %% "weaver-cats"       % weaverVersion % Test,
+            "org.typelevel" %% "weaver-scalacheck" % weaverVersion % Test
+          )
+        )
+      }
+    }
+  }
+}
--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -23,27 +23,5 @@ object ProjectPlugin extends AutoPlugin {
     def module(s: String): Project =
       Project(s, file(jarName(s)))
         .settings(name := jarName(s))
-
-    implicit class ProjectOps(p: Project) {
-      def withCats: Project =
-        p
-          .settings(libraryDependencies += "org.typelevel" %% "cats-effect" % "3.6.3")
-
-      def withZio: Project =
-        p
-          .settings(libraryDependencies += "dev.zio" %% "zio" % "2.1.9")
-
-      def withTesting: Project = {
-        val weaverVersion =
-          "0.10.0"
-
-        p.settings(
-          libraryDependencies ++= Seq(
-            "org.typelevel" %% "weaver-cats"       % weaverVersion % Test,
-            "org.typelevel" %% "weaver-scalacheck" % weaverVersion % Test
-          )
-        )
-      }
-    }
   }
 }
